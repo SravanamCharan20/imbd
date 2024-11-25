09daede1521275e089f61d71fca6cd425b1e3d39
Sravanam charan <155254854+SravanamCharan20@users.noreply.github.com>
2024-11-25 22:16:12 +0530

    Update head.jsx

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>IMDb Clobe</title>
+      <title>CineVerse</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="This is the IMDb clone website" />
       <link rel="icon" href="/favicon.ico" />
