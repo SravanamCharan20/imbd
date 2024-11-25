a2b88c1d825b76213cac7a47d32723a68930bea7
Sravanam charan <155254854+SravanamCharan20@users.noreply.github.com>
2024-11-25 22:17:35 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,9 +16,9 @@ export default function Header() {
         <Link href="/">
           <h2 className="text-2xl">
             <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
-              IMDb
+              Cine
             </span>
-            <span className="text-xl hidden sm:inline">Clone</span>
+            <span className="text-xl hidden sm:inline">Verse</span>
           </h2>
         </Link>
       </div>
